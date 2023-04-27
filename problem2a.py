24e03648da85e354aa1fa9fd692957e89e363737
Patrick <48323051+thepossumen@users.noreply.github.com>
2023-04-27 08:12:32 +0200

    file rename

--- problem2a.py
+++ problem2a.py
@@ -8,7 +8,7 @@ sc = SparkContext(master = 'local[4]')
 
 data = sc.textFile('/data/2023-DAT470-DIT065/data-assignment-3-1M.dat')
 
-def extract_values(line):
+def extract_value(line):
     _, _, value = line.split("\t")
     value = float(value)
     return value, value**2, 1, value, value
@@ -18,7 +18,7 @@ def sum_vals(a, b):
     v2, s2, c2, minv2, maxv2 = b
     return v1 + v2, s1 + s2, c1 + c2, min(minv1,minv2), max(maxv1, maxv2)
 
-sums_and_count = data.map(extract_values).reduce(sum_vals)
+sums_and_count = data.map(extract_value).reduce(sum_vals)
 
 print(sums_and_count)
 val_sum, sq_sum, N, minv, maxv = sums_and_count 
